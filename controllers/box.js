ef63dcfc44004c522ba638246bd8cd47558a4c82
mitali mori <mitalimori33@gmail.com>
2024-08-01 16:56:40 +0530

    Create

--- controllers/box.js
+++ controllers/box.js
@@ -2,25 +2,25 @@ var mongoose = require('mongoose')
 var Box = require('../models/Box-Owner/box.js')
 var jwt = require('jsonwebtoken')
 var Owner = require('../models/Box-Owner/owner.js')
-const cloudinary = require('../routes/Box-Router/cloudinary.js')
+const cloudinary = require('../routes/Box-Router/cloudinary.js');
 exports.addBox = async (req, res) => {
     try {
-        var { boxName, street, city, state, area, pinCode, country, morningPrice, nightPrice, contact } = req.body;
+        var {boxName, images ,street ,city,state , area,pinCode,country, morningPrice , nightPrice , contact } = req.body
 
         if (!boxName || !street || !city || !state || !area || !pinCode || !country || !morningPrice || !nightPrice || !contact) {
-            throw new Error('All fields are required');
+            throw new Error('Filds are required')
         }
-
-        var id = await jwt.verify(req.headers.auth, 'Owner');
+        var id = await jwt.verify(req.headers.auth, 'Owner')
         if (!id) {
-            throw new Error("Token must be provided");
+            throw new Error("token must be provided")
         }
+        var ownerId = id
 
-        var checkStatus = await Owner.findById(id);
-        let status = checkStatus.status;
+        var checkStatus = await Owner.findById(id)
+        let status = checkStatus.status
 
         if (status !== "approved") {
-            throw new Error(`You are not able to add a box because your status is ${status}`);
+            throw new Error(`You are not able to add box because your status is ${status}`);
         }
 
         var images = req.files ? await Promise.all(req.files.map(async (file) => {
@@ -29,11 +29,11 @@ exports.addBox = async (req, res) => {
         })) : [];
 
         var addBoxData = new Box({
-            ownerId: id,
+            ownerId,
             boxName,
             images,
             contact,
-            address: {
+            address : {
                 street,
                 area,
                 city,
@@ -41,40 +41,40 @@ exports.addBox = async (req, res) => {
                 pinCode,
                 country
             },
-            opening: {
-                morning: {
-                    morningPrice
+            opning:{
+                morning : {
+                    morningPrice : morningPrice
                 },
-                night: {
-                    nightPrice
+                night : {
+                    nightPrice : nightPrice
                 }
             }
-        });
+        })
 
-        await addBoxData.save();
+        await addBoxData.save()
 
         res.status(200).json({
             status: 'Success',
-            message: 'Box created successfully',
+            message: 'Box Create Successfully',
             data: addBoxData
-        });
+        })
+
 
     } catch (error) {
         res.status(401).json({
             status: 'Failed',
             message: error.message
-        });
+        })
     }
-};
-
+}
 
 
 
 exports.updateBox = async (req, res) => {
     try {
-        const { boxName, street, city, state, area, pinCode, country, morningPrice, nightPrice } = req.body;
+        const { boxName, street, city, state, pinCode, country, morningPrice, nightPrice } = req.body;
         const token = req.headers.auth;
-
+        
         if (!token) {
             throw new Error("Token must be provided");
         }
@@ -96,13 +96,12 @@ exports.updateBox = async (req, res) => {
             images,
             address: {
                 street,
-                area,
                 city,
                 state,
                 pinCode,
                 country
             },
-            opening: {
+            opning: {
                 morning: {
                     morningPrice
                 },
@@ -120,7 +119,7 @@ exports.updateBox = async (req, res) => {
 
         res.status(200).json({
             status: 'Success',
-            message: 'Box updated successfully',
+            message: 'Box Updated Successfully',
             data: updatedBox
         });
 
@@ -131,7 +130,6 @@ exports.updateBox = async (req, res) => {
         });
     }
 };
-
 
 
 exports.removeBox = async (req,res) => {
